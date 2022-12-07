047e2093bfc85c62bbdb4a6438987a542b8f112d
sauhempire <sauhempire@gmail.com>
2022-12-07 14:51:20 +0530

    update

---
+++ 1.c
@@ -0,0 +1,14 @@
+
+   #include<stdio.h>
+   int main()
+ {
+    int n,i=1;
+    printf("Enter your number\n");
+    scanf("%d",&n);
+    while(i<=n)
+    {
+        printf("MySirG\t");
+        i++;
+    }
+    return 0;
+ }
